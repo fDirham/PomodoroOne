94e4c8c0b84bc084a59124b7c65eaeb3b4f9db2e
fDynamo <fontaine.dynamo@gmail.com>
2024-07-29 16:57:51 -0700

    first + show icon and timer text in custom view using app delegates and uikit

---
+++ PomodoroOne/MenuBarView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuBarView.swift
+//  PomodoroOne
+//
+//  Created by Fajar Dirham on 7/29/24.
+//
+
+import SwiftUI
+
+struct MenuBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuBarView()
+}
---
+++ PomodoroOne/StatusBarController.swift
@@ -0,0 +1,8 @@
+//
+//  StatusBarController.swift
+//  PomodoroOne
+//
+//  Created by Fajar Dirham on 7/29/24.
+//
+
+import Foundation
