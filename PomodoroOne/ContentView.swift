5893b7916bc76e8efd4e3592b093428b2bdd5ddb
fDynamo <fontaine.dynamo@gmail.com>
2024-07-29 11:26:43 -0700

    Initial Commit

---
+++ PomodoroOne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PomodoroOne
+//
+//  Created by Fajar Dirham on 7/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PomodoroOne/PomodoroOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroOneApp.swift
+//  PomodoroOne
+//
+//  Created by Fajar Dirham on 7/29/24.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
