e77a4dc8b4da99bdb9ebf545ba454ed55ea342e3
fDynamo <fontaine.dynamo@gmail.com>
2024-07-30 11:00:57 -0700

    simple static view of content view

--- PomodoroOne/ContentView.swift
+++ PomodoroOne/ContentView.swift
@@ -8,17 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isPaused: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Button("Exit") {
-                exit(0)
+            Spacer()
+            VStack(spacing:5) {
+                Text("12:34")
+                    .font(.system(size: 32))
+                Button(action: {isPaused.toggle()}){
+                    Image(systemName: isPaused ?  "play.fill" :"pause.fill")
+                        .resizable()
+                        .frame(width: 18, height: 18)
+                }
+                .buttonStyle(PlainButtonStyle())
+            }
+            Spacer()
+            HStack{
+                Spacer()
+                Menu {
+                    Button("Settings", action: {
+                        print("TODO")
+                    })
+                    Button("Quit", action: {exit(0)})
+                } label: {
+                    Image(systemName: "gearshape.fill")
+                        .resizable()
+                        .frame(width: 15, height: 15)
+                }
+                .menuStyle(BorderlessButtonMenuStyle())
+                .menuIndicator(.hidden)
+                .fixedSize()
             }
         }
         .padding()
+        .frame(width: 168, height: 143)
     }
 }
 
--- PomodoroOne/MenuBarView.swift
+++ PomodoroOne/MenuBarView.swift
@@ -11,7 +11,10 @@ struct MenuBarView: View {
     var body: some View {
         HStack{
             Text("20:24")
+                .font(.system(size: 14))
             Image("menu-bar-icon__on")
+                .resizable()
+                .frame(width: 17, height: 17)
         }
     }
 }
